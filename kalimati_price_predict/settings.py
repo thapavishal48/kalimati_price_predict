1ca9ac8df4e5daeaddf3a38161fb11abc79b9ca1
Vishal thapa <thapavishal48@gmail.com>
2020-10-07 10:01:55 +0545

    Add static files for deployment

--- kalimati_price_predict/settings.py
+++ kalimati_price_predict/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'kalimati_price_predict.urls'
 
@@ -127,3 +126,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
