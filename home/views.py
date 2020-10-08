ec4f807daee65dc09e57df69b9d9329e2c75ee86
Vishal thapa <thapavishal48@gmail.com>
2020-10-08 10:12:46 +0545

    graph path updated

--- home/views.py
+++ home/views.py
@@ -54,7 +54,7 @@ def pickle_training_testing(request):
     fig.update_layout(title=veg_name,
                       yaxis_zeroline=False, xaxis_zeroline=False)
 
-    fig.write_html('./graph_show/veg_graph.html', auto_open=True)
+    fig.write_html('https://kalimati-tarkari-price-predict.herokuapp.com/graph_show/veg_graph.html', auto_open=True)
 
     return HttpResponseRedirect('/select/')
 
